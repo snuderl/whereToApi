e84236679e65c877409d2a6f609d24a96e81acd7
snuderl <snuderl@gmail.com>
2016-03-09 21:24:37 +0000

    Elastsicbeanstalk conf.

---
+++ whereToApi/prod_settings.py
@@ -0,0 +1,14 @@
+from settings import *
+
+DEBUG = False
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    'NAME': 'whereTo',
+    'HOST': 'localhost',
+    'PORT': '5432',
+    'USER': 'whereTo',
+    'PASSWORD': 'msm16531'
+  }
+}
