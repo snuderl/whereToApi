c62d51d067d2164231a6b8b3e804902a0cc522e1
snuderl <snuderl@gmail.com>
2016-03-10 00:25:32 +0100

    Update prod settings default.

--- whereToApi/wsgi.py
+++ whereToApi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whereToApi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whereToApi.prod_settings")
 
 application = get_wsgi_application()
