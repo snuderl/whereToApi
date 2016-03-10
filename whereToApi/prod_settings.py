c124f71c8bf1ac08cd239cbb0781f03f89de369a
snuderl <snuderl@gmail.com>
2016-03-10 22:03:16 +0100

    Settingsu pdated.

--- whereToApi/prod_settings.py
+++ whereToApi/prod_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
   'default': {
     'ENGINE': 'django.contrib.gis.db.backends.postgis',
     'NAME': 'whereTo',
-    'HOST': 'localhost',
+    'HOST': 'whereto.cwcxg0riymqz.eu-central-1.rds.amazonaws.com',
     'PORT': '5432',
     'USER': 'whereTo',
     'PASSWORD': 'msm16531'
