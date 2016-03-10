781d806013a33a786890f3b0dfb27a70e71434a9
snuderl <snuderl@gmail.com>
2016-03-10 22:47:55 +0100

    Update settings.

--- whereToApi/settings.py
+++ whereToApi/settings.py
@@ -111,5 +111,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static/')
+
