f86366021dcf091e399972e7e8b809a93612cba2
snuderl <snuderl@gmail.com>
2016-03-12 18:49:52 +0100

    Allow geocoding queries.

--- events/views.py
+++ events/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
 from .models import Place, Event
 from rest_framework import viewsets
 from rest_framework_gis.filters import DistanceToPointFilter
-from rest_framework.generics import ListAPIView
 from .serializers import EventSerializer, PlaceSerializer
+from rest_framework import filters
+import geocoder
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
+
+
+DEFAULT_DISTANCE = 4000
+
+
+def location_name_filter(qs, query_params):
+    if 'location_name' in query_params:
+        g = geocoder.google(query_params['location_name'])
+        point = Point(*g.latlng)
+
+        distance = query_params.get('distance', str(DEFAULT_DISTANCE))
+        print(point)
+        return qs.filter(coords__distance_lt=(point, Distance(m=distance)))
+    return qs
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
@@ -13,13 +29,33 @@ class PlaceViewSet(viewsets.ModelViewSet):
     distance_filter_convert_meters = True
     filter_backends = (DistanceToPointFilter, )
     bbox_filter_include_overlapping = True
+    filter_backends = (filters.DjangoFilterBackend,)
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = self.queryset
+        queryset = location_name_filter(queryset, self.request.query_params)
+        return queryset
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
+    queryset = Place.objects.all()
     serializer_class = EventSerializer
     distance_filter_field = 'coords'
     distance_filter_convert_meters = True
     filter_backends = (DistanceToPointFilter, )
     bbox_filter_include_overlapping = True
+    filter_backends = (filters.DjangoFilterBackend,)
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = self.queryset
+        queryset = location_name_filter(queryset, self.request.query_params)
+        return queryset
 
