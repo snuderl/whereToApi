d33d32416a2328eb77f90d3480159d4203454783
snuderl <snuderl@gmail.com>
2016-03-12 21:59:44 +0100

    Simplify point representation.

--- events/serializers.py
+++ events/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 from .models import Place, Event
+from rest_framework_gis.fields import GeometryField
+
+
+class SimplePointField(GeometryField):
+    def to_representation(self, value):
+        if isinstance(value, dict) or value is None:
+            return value
+        # we expect value to be a GEOSGeometry instance
+        return value.coords
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+    coords = SimplePointField()
+
     class Meta:
         model = Place
 
 
 class EventSerializer(serializers.ModelSerializer):
+    coords = SimplePointField()
+
     class Meta:
         model = Event
 
