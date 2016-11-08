40b426dfda0d20a8cadea884bfb240bbc4f78538
Blaž Šnuderl <snuderl@gmail.com>
2016-11-08 21:29:58 +0700

    Improve admin.

--- events/admin.py
+++ events/admin.py
@@ -3,11 +3,16 @@ from events.models import Place, Event
 
 
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+  list_display = ('name', 'city')
+  search_fields = ('name', )
 
 
 class EventAdmin(admin.ModelAdmin):
-    pass
+  list_display = ('name', 'place_name', 'start_time', 'end_time')
+  search_fields = ('name', 'place__name')
+
+  def place_name(self, obj):
+    return obj.place.name
 
 
 admin.site.register(Place, PlaceAdmin)
