725cadbe70b2beadd7ba6d88e5655c79f138988d
Blaž Šnuderl <snuderl@gmail.com>
2016-11-08 21:19:09 +0700

    Add admin site

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from events.models import Place, Event
 
-# Register your models here.
+
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Event, EventAdmin)
